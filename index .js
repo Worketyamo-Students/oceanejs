166621cb14736a2a24ffd32962383b91e9924613
maeva501 <maevaoceane79@gmail.com>
2025-03-10 15:52:12 +0100

    notion d'objet

---
+++ index .js
@@ -0,0 +1,21 @@
+//let film ={
+   // title: "a contre sens" ,
+    //realiateur : "maryline" ,
+   // anneeDeSortie : 2020   ,
+   // acteurs: ["nicole", "gabrielle", "varonne"]
+//}
+//console.log(film)
+//console.log(Object.values(film.acteurs[0]))
+//exo2
+//let ordinateur ={
+    //marque: "a contre sens" ,
+    //modele : "maryline" ,
+    //processeur : 2020   ,
+    //ram: ["nicole", "gabrielle", "varonne"],
+    //stockage :0
+//}
+//for( const prop in ordinateur) {
+   // console.log(`ordinateur.${prop} = ${ordinateur[prop]}`)
+//}
+//console.log(Object.keys(ordinateur))
+
